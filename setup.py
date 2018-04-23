b84f0cd58636b7a74febd33ce4ed4b867b71e120
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-04-23 13:10:46 -0700

    hmm

--- setup.py
+++ setup.py
@@ -2,6 +2,21 @@ import setuptools
 
 setuptools.setup(
     name='pipenvtest',
-    packages=setuptools.find_packages('src'),
-    package_dir={'': 'src'},
+    packages=setuptools.find_packages('python'),
+    package_dir={'': 'python'},
+    install_requires=[
+        'ccstudiodss',
+        'pytest',
+        'pytest-twisted',
+        'pytest-qt',
+        'python-can',
+        'pyqt5',
+        'twisted',
+        'pypiwin32;platform_system=="Windows"',
+    ],
+    entry_points={
+        'pytest11': [
+            'pipenvtest_cli = pipenvtest.cli:f',
+        ]
+    },
 )
--- src/pipenvtest/core.py
+++
@@ -1,1 +0,0 @@
-note = 'this is pipenvtest'
